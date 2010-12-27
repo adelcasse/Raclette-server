77ff001ed9a4e12824afcdcff809386ae84b4051
Arnaud Delcasse <delcasse@gmail.com>
2010-12-27 18:22:40 +0100

    Basic creation method to be continued to be able to merge an existing file into it's already existing record.

--- app/controllers/scanned_files_controller.rb
+++ app/controllers/scanned_files_controller.rb
@@ -8,7 +8,16 @@ class ScannedFilesController < ApplicationController
   end
 
   def show
-    @scanned_file = ScannedFile.criteria.id(params[:id]).to_a
-    respond_with @scanned_file
+    @scanned_file = ScannedFile.criteria.id(params[:id])
+    respond_with @scanned_file.to_a
   end
+
+  def create
+    #TODO: Add some controls to merge with an existing file if it has the same hash or same host/directory
+    #TODO: control the creation and generate an answer depending on the success of the operation. Sucess => send back the object in database. Error => Send back the error
+    @scanned_file = ScannedFile.new(params[:scanned_file])
+    @scanned_file.save
+    respond_with @scanned_file.to_a
+  end
+
 end
