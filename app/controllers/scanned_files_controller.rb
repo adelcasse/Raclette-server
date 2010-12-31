db5bf9850b862e380c0e7c6cdaa13f5a48a1aa71
Arnaud Delcasse <delcasse@gmail.com>
2010-12-31 11:12:12 +0100

    Adding a new ScannedFile with existing file path doesn't insert that new path (right merge) : still have to handle when the hash is changed between 2 scans.

--- app/controllers/scanned_files_controller.rb
+++ app/controllers/scanned_files_controller.rb
@@ -14,13 +14,14 @@ class ScannedFilesController < ApplicationController
 
 
   def create
-
+    #TODO: if the file already exists but its hash has been changed, we have to discontinue it and create a new scanned_file document with the new hash
+    #TODO: Implement hosts de pending on the final schema chosed
     scanned_file = params[:scanned_file]
     single_file = scanned_file[:single_files][0]
 
     @scanned_file = ScannedFile.new(scanned_file)
 
-    @existing_file = ScannedFile.first(:conditions => {"scanned_files.file_name" => single_file[:file_name]})
+    @existing_file = ScannedFile.where("single_files.file_name" => single_file[:file_name]).and("single_files.directory" => single_file[:directory]).first
     if @existing_file.nil?
       # There isn't any file matching the path of the new scanned file
       @new_file = ScannedFile.find_or_initialize_by(:filehash => scanned_file[:filehash])
@@ -35,37 +36,4 @@ class ScannedFilesController < ApplicationController
       respond_with @existing_file
     end
   end
-
-
-  def create2
-    @scanned_file = ScannedFile.new(params[:scanned_file])
-
-    @host = Host.find_or_initialize_by(:name => params[:host][:name],
-                                        :protocol => params[:host][:protocol])
-    @host.save
-
-    @single_file = SingleFile.find_or_initialize_by(:file_name => @scanned_file.single_files[0].file_name,
-                                                    :directory => @scanned_file.single_files[0].directory)
-    #@single_file.save
-    
-    if not @single_file.scanned_file.nil?
-    then
-      @new_file = @single_file.scanned_file
-      @new_file.merge_with @scanned_file
-    elsif not @scanned_file.filehash.nil?
-    then
-      @new_file = ScannedFile.find_or_initialize_by(:filehash => @scanned_file.filehash)
-      @new_file.merge_with @scanned_file
-      @new_file.single_files << @single_file
-    else
-      @new_file = ScannedFile.new
-      @new_file.filehash = @scanned_file.filehash if not @scanned_file.filehash.nil?
-      @new_file.merge_with @scanned_file
-      @new_file.single_files << @single_file
-    end
-
-    @new_file.save
-    respond_with @new_file
-  end
-
 end
