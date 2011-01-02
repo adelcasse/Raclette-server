75048b5170c079aa60b1ad489dcb4a603ff77f28
Arnaud Delcasse <delcasse@gmail.com>
2011-01-02 21:39:57 +0100

    Added unmerge functionality to handle files changing their hash. The global creation alrotithm should now be fine and I need to improve it on details now

--- app/controllers/scanned_files_controller.rb
+++ app/controllers/scanned_files_controller.rb
@@ -14,8 +14,6 @@ class ScannedFilesController < ApplicationController
 
 
   def create
-    #TODO: if the file already exists but its hash has been changed, we have to discontinue it and create a new scanned_file document with the new hash
-    #TODO: Implement hosts de pending on the final schema chosed
     scanned_file = params[:scanned_file]
     single_file = scanned_file[:single_files][0]
     host = params[:host]
@@ -28,20 +26,29 @@ class ScannedFilesController < ApplicationController
     @existing_file = ScannedFile.where("single_files.file_name" => single_file[:file_name]).and("single_files.directory" => single_file[:directory]).first
     if @existing_file.nil?
       # There isn't any file matching the path of the new scanned file
-      @new_file = ScannedFile.find_or_initialize_by(:filehash => scanned_file[:filehash])
+      @new_file = ScannedFile.find_or_create_by(:filehash => scanned_file[:filehash])
       @new_file.merge_with @scanned_file
       @new_file.single_files.create single_file
       @new_file.single_files[0].host = @host
+      @new_file.single_files[0].save
       @new_file.save
       respond_with @new_file
     else
       # This file is already in the database
-      if @existing_file.filehash == scanned_file[:file_hash]
+      if @existing_file.filehash == scanned_file[:filehash]
         @existing_file.merge_with @scanned_file
         @existing_file.save
+        respond_with @existing_file
       else
+	@existing_file.single_files.destroy_all(:conditions => {:file_name => single_file[:file_name], :directory => single_file[:directory]})
+	@existing_file.save
+        @new_file = ScannedFile.find_or_create_by(:filehash => scanned_file[:filehash])
+        @new_file.merge_with @scanned_file
+        @new_file.single_files.create single_file
+        @new_file.single_files[0].host = @host
+        @new_file.save
+        respond_with @new_file
       end
-      respond_with @existing_file
     end
   end
 end
