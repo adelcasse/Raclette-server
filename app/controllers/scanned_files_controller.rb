efdc45a7c7a4c76435681a790d3841a19a222d22
Arnaud Delcasse <delcasse@gmail.com>
2011-01-03 02:14:44 +0100

    Code cleaning

--- app/controllers/scanned_files_controller.rb
+++ app/controllers/scanned_files_controller.rb
@@ -26,12 +26,7 @@ class ScannedFilesController < ApplicationController
     @existing_file = ScannedFile.where("single_files.file_name" => single_file[:file_name]).and("single_files.directory" => single_file[:directory]).first
     if @existing_file.nil?
       # There isn't any file matching the path of the new scanned file
-      @new_file = ScannedFile.find_or_create_by(:filehash => scanned_file[:filehash])
-      @new_file.merge_with @scanned_file
-      @new_file.single_files.create single_file
-      @new_file.single_files[0].host = @host
-      @new_file.single_files[0].save
-      @new_file.save
+      @new_file = create_new_file_from(@scanned_file, single_file)
       respond_with @new_file
     else
       # This file is already in the database
@@ -42,13 +37,22 @@ class ScannedFilesController < ApplicationController
       else
 	@existing_file.single_files.destroy_all(:conditions => {:file_name => single_file[:file_name], :directory => single_file[:directory]})
 	@existing_file.save
-        @new_file = ScannedFile.find_or_create_by(:filehash => scanned_file[:filehash])
-        @new_file.merge_with @scanned_file
-        @new_file.single_files.create single_file
-        @new_file.single_files[0].host = @host
-        @new_file.save
-        respond_with @new_file
+        @new_file = create_new_file_from(@scanned_file, single_file)
+	respond_with @new_file
       end
     end
   end
+
+
+  private
+
+  def create_new_file_from(scanned_file, single_file)
+      @new_file = ScannedFile.find_or_create_by(:filehash => scanned_file.filehash)
+      @new_file.merge_with scanned_file
+      @new_file.single_files.create single_file
+      @new_file.single_files[0].save
+      @new_file.save
+      return @new_file
+  end
+
 end
